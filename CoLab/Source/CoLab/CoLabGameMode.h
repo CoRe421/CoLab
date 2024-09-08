93a5e1f290a82edf1d02566d332765765216e0e8
Cory Reardon <cjreardon@gmail.com>
2024-09-08 14:27:55 -0700

    Initial project commit

---
+++ CoLab/Source/CoLab/CoLabGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CoLabGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACoLabGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACoLabGameMode();
+};
+
+
+
